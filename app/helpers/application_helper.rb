306061b4dfd581710b2f7d39c98459821bb51695
scman1 <scman1@c17be5b9-c8e9-4d1b-b8cc-0817c9757c39>
2012-08-02 13:17:10 +0000

    Added select field for server type
    git-svn-id: https://svn.cs.cf.ac.uk/BioVeL/TavernaLite/Branches/tliteR3@26 c17be5b9-c8e9-4d1b-b8cc-0817c9757c39

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def button_select(model_name, target_property, button_source)
+    html=''
+    list=button_source.sort
+    if list.count > 0
+      html << label (model_name, target_property) << '<br />'
+    end
+    if list.count < 4
+      list.each{|x|
+        html << radio_button(model_name, target_property, x[1])
+        html << h(x[0])
+        html << '<br />'
+      }
+    else
+      html << select(model_name, target_property,list)
+    end
+    
+    return html.html_safe
+  end
+  
 end
--- app/models/credential.rb
+++ app/models/credential.rb
@@ -4,6 +4,7 @@ class Credential < ActiveRecord::Base
   validates_presence_of :url
   validates_presence_of :login
   validates_presence_of :password
+  validates_presence_of :server_type, :message => 'Please select a server type'
   # only one taverna server is enabled at a time
   def self.get_taverna_uri
     find_by_server_type_and_default_and_in_use("ts",true,true).url
