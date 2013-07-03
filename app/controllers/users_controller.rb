2cecb14c78f09d4d4b445c3bce7c2ff2bbbbeb1a
Robert Haines <rhaines@manchester.ac.uk>
2013-07-03 15:33:39 +0100

    [SERVINF-84] Fix whitespace errors in three missed files.
    Not sure how these were missed... :-/

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,29 +6,29 @@ class UsersController < ApplicationController
     guest.id = 0
     guest.user_statistic = UserStatistic.where(:user_id=>0)[0]
     @biovel_users << guest
-    @user_statistic = UserStatistic.all 
+    @user_statistic = UserStatistic.all
   end
 
-  def new  
-    @user = User.new  
-  end  
-    
-  def create  
-    @user = User.new(params[:user])  
-    if @user.save  
-      redirect_to root_url, 
-        :notice=>"You are now Registered, Login to start using BioVeL Portal!"  
-    else  
-      render "new"  
-    end  
-  end 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to root_url,
+        :notice=>"You are now Registered, Login to start using BioVeL Portal!"
+    else
+      render "new"
+    end
+  end
   # GET /workflows/1/edit
   def edit
     @user = User.find(params[:id])
   end
   def update
     @user = User.find(params[:id])
-     
+
     respond_to do |format|
       if @user.update_attributes(params[:user])
         format.html { redirect_to '/users', :notice => 'User was successfully updated.' }
