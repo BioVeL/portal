a989cf1104b6321251c65fdac33375b4f82990da
Robert Haines <rhaines@manchester.ac.uk>
2013-08-01 16:23:42 +0100

    Clean up asset inclusion for production.
    Move the jquery.js precompile step to the application config so that it is set
    for all environments. The other css files are included in application.css when
    compiled so can be removed from the list in the production config.

--- config/application.rb
+++ config/application.rb
@@ -106,5 +106,7 @@ module TliteR3
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile += ["jquery.js"]
   end
 end
